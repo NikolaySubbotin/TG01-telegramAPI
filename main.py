bc3461f7f01285bfa99e738b4e575ec97203eefd
NikolaySubbotin <n.subbotin1986@gmail.com>
2024-10-06 23:25:08 +0300

    add TG02

--- main.py
+++ main.py
@@ -1,15 +1,40 @@
 import asyncio
-from flask import Flask, request, render_template
 import requests
 from aiogram import Bot, Dispatcher, F
-from aiogram.types import Message
+from aiogram.types import Message, FSInputFile
 from aiogram.filters import CommandStart, Command
 from config import TOKEN, OPENWEATHER_API_KEY
+from gtts import gTTS
+from googletrans import Translator
+import os
 import random
 
 
 bot = Bot(token=TOKEN)
 dp = Dispatcher()
+
+@dp.message(Command('audio'))
+async def audio(message: Message):
+    await bot.send_chat_action(message.chat.id, 'upload_audio')
+    audio = FSInputFile('Суздальская Ночь.mp3')
+    await bot.send_audio(message.chat.id, audio)
+
+@dp.message(Command('history'))
+async def history(message: Message):
+    await bot.send_chat_action(message.chat.id, 'upload_voice')
+    history_list = [
+        "Чем уникален Суздаль? В городе сохранились многочисленные памятники архитектуры, которые позволяют ощутить дух средневековой Руси. Здесь можно посетить Суздальский кремль, Спасо-Евфимиев монастырь, Покровский монастырь, а также храмы и объекты культурного наследия, каждый из которых имеет свою уникальную историю.",
+        "Почему город Суздаль так назвали? Так, впервые город упоминается как Суждаль в Лаврентьевской летописи 1024 года, что могло обозначать (основатель судил быть тут городу). Если предположить греческое происхождение, то (Суз дулус) в переводу с греческого означает (раб твой).",
+        "Когда и кем был основан город Суздаль? Первое упоминание Суздаля в летописях происходит в 1024 году из-за восстания волхвов. Согласно (Повести временных лет), крестьяне взбунтовались из-за неурожайного года, вызванного засухой. Чуть позже Суздаль становится вотчиной Владимира Мономаха, который уделяет колоссальное внимание усилению и укреплению города."
+    ]
+    rand_his = random.choice(history_list)
+    await message.answer(f'Это история города Суздаль: {rand_his}')
+
+    tts = gTTS(text=rand_his, lang='ru')
+    tts.save("history.ogg")
+    audio = FSInputFile('history.ogg')
+    await bot.send_voice(message.chat.id, audio)
+    os.remove("history.ogg")
 
 
 def get_weather(city):
@@ -33,17 +58,23 @@ async def photo(message: Message):
     rand_photo = random.choice(list)
     await message.answer_photo(photo=rand_photo, caption="Фото г.Суздаль")
 
+@dp.message(F.photo)
+async def react_photo(message: Message):
+    await bot.download(message.photo[-1], destination=f'tmp/{message.photo[-1].file_id}.jpg')
+
+
+
 @dp.message(F.text == "Что такое бот?")
 async def bottext(message: Message):
     await message.answer('Бот - это программа, выполняющая автоматические заранее настроенные повторяющиеся задачи')
 
 @dp.message(Command('help'))
 async def help(message: Message):
-    await message.answer("Список комманд:\n/start\n/help\n/photo\n/weather")
+    await message.answer("Список комманд: \n /start \n /help \n /photo \n /weather \n /audio \n /history")
 
 @dp.message(CommandStart())
 async def start(message: Message):
-    await message.answer("Привет я бот!")
+    await message.answer(f"Привет {message.from_user.first_name}! я бот про красивый город Суздаль")
 
 
 @dp.message(Command('weather'))
@@ -57,6 +88,17 @@ async def weather(message: Message):
         await message.answer(weather_info)
     else:
         await message.answer("Пожалуйста, укажите город после команды /weather.\nНапример: /weather Москва")
+
+
+translator = Translator()
+# Обработка обычных текстовых сообщений, не являющихся командами
+@dp.message()
+async def translate(message: Message):
+    # Проверяем, если это не команда (не начинается с '/')
+    if not message.text.startswith('/'):
+        translated = translator.translate(message.text, src='ru', dest='en')  # Переводим с русского на английский
+        await message.answer(translated.text)
+
 async def main():
     await dp.start_polling(bot)
 
